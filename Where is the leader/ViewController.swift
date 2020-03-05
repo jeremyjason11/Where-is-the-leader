63548ad1567525053d62200fb8410d03a5585ff8
Jeremy Jason <booya1402@gmail.com>
2020-03-05 12:39:52 +0700

    Initial Commit

---
+++ Where is the leader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Where is the leader
+//
+//  Created by Jeremy Jason on 05/03/20.
+//  Copyright © 2020 Jeremy Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
